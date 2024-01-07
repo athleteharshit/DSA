b67f41070b15800c0daa831fba725615835304dd
Harshit gupta <62887518+athleteharshit@users.noreply.github.com>
2024-01-07 17:47:28 +0000

    u

--- array/assignment/2Darray.cpp
+++ array/assignment/2Darray.cpp
@@ -48,13 +48,58 @@ void rowWaveMatrix(vector<vector<int>> mat, int row, int col)
     }
 }
 
+// 54. Spiral Matrix (leet code)
+vector<int> spiralMatrix(vector<vector<int>> matrix) {
+    vector<int> ans;
+    int rowStart = 0;
+    int rowEnd = matrix.size() - 1;
+    int colStart = 0;
+    int colEnd = matrix[0].size() - 1;
+    int count = 0;
+    int totalElement = matrix.size() * matrix[0].size();
+    while(count < totalElement) {
+         for(int i = colStart; i <= colEnd && count < totalElement; i++) {
+            ans.push_back(matrix[rowStart][i]);
+            count++;
+         }
+         rowStart++;
+
+         for(int i = rowStart; i <= rowEnd && count < totalElement; i++) {
+            ans.push_back(matrix[i][colEnd]);
+            count++;
+         }
+         colEnd--;
+
+         for(int i = colEnd; i >= colStart && count < totalElement; i--) {
+            ans.push_back(matrix[rowEnd][i]);
+            count++;
+         }
+         rowEnd--;
+
+         for(int i = rowEnd; i >= rowStart && count < totalElement; i--){
+            ans.push_back(matrix[i][colStart]);
+            count++;
+         }
+         colStart++;
+    }
+    return ans;
+}
+
 int main()
 {
-    vector<vector<int>> mat = {
+    // [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+    // vector<vector<int>> mat = {
+    //     {1, 2, 3, 4, 5},
+    //     {6, 7, 8, 9, 10},
+    //     {11, 12, 13, 14, 15},
+    //     {16, 17, 18, 19, 20}
+    //     };
+      vector<vector<int>> mat = {
         {1, 2, 3, 4},
         {5, 6, 7, 8},
-        {9, 10, 11, 12}};
+        {9, 10, 11, 12}
+        };
 
-    rowWaveMatrix(mat, mat.size(), mat[0].size());
+    spiralMatrix(mat);
     return 0;
 }
