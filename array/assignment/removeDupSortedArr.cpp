ad5d1571149d8a97b949396b30aaa849b22c9bf3
Harshit gupta <62887518+athleteharshit@users.noreply.github.com>
2024-01-12 19:41:46 +0000

    update

--- array/assignment/removeDupSortedArr.cpp
+++ array/assignment/removeDupSortedArr.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // Remove Duplicates from Sorted Array leetcode: 26
- int removeDuplicates(vector<int>& nums) {
+int removeDuplicates(vector<int>& nums) {
       int j = 0;
       for(int i = 1; i < nums.size(); i++) {
           if(nums[j] != nums[i]) {
@@ -11,6 +11,27 @@ using namespace std;
           }
       }
     return j+1;
+}
+
+// Maximum Average Subarray I (leetcode: 643) using sliding window problem
+double findAvg(vector<int>& nums, int&k) {
+        int sum = 0;
+        int i = 0;
+        int j = k;
+
+        for(int l = i; l < j; l++) {
+            sum += nums[l];
+        }
+
+        int maxSum = sum;
+        while(j < nums.size()) {
+            sum -= nums[i++];
+            sum += nums[j++];
+            maxSum = max(maxSum, sum);
+        }
+
+        double ans = maxSum / (double)k;
+    return ans;
 }
 
 int main() {
